1d90c506904cf261cb38a56ebf7901930fdea06f
pashaSok <https://github.com/pashaSok>
2019-10-13 21:12:18 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -34,13 +34,39 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+	'-----':  '0',
+	'**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+	/*var word =[];
+	word = expr.split('**********');
+	var str = word.join('');*/
+	let letters=[];
+    for(let i = 0; i < expr.length; i = i + 10){
+		letters.push(expr.slice(i, i + 10));
+	}
+	var newStr = letters.join(' ');
+	var encodeStr = newStr.replace(/00/gi, '').replace(/10/gi,'.').replace(/11/gi,'-');
+	let encoderArr = encodeStr.split(' ');
 
+	var nenewStr='';
+
+	let expMorse = {'**********' : " "};
+    for (let name in MORSE_TABLE) {
+        expMorse[Object.values(MORSE_TABLE[name])] = name;
+	}
+	for( let j=0; j <= encoderArr.length; j++){
+		for(let key in expMorse){
+			if(encoderArr[j] == expMorse[key]){
+			
+				nenewStr+=key;
+			}
+		}
+	}
+
+	return nenewStr;
+}
 module.exports = {
     decode
 }
